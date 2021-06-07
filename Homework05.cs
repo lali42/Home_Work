a7415a4e107efcecfef878165cfc23216b75274c
lali42 <lalita.su.61@ubu.ac.th>
2021-06-08 03:15:35 +0700

    homework05

---
+++ Homework05.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Home_work
+{
+    class CtrlLED
+    {
+        static void Main(string[] args){
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,22 +2,12 @@
 
 namespace Home_Work
 {
-    public interface IHomework04
-    {
-        string GetDivisibleBy5Text(string text);
-    }
-    class Homework4
-    {
-        
-    }
+
     class Program
     {
         // static void Main(string[] args)
         // {
-        //     // string name;
-        //     // Console.Write("Enter Your Name : ");
-        //     // name = Console.ReadLine();
-        //     // Console.Write("Hello, "+name);
+
         // }
     }
 }
